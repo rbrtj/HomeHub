5f022998506ee08fc2a310e8f40adbd93cd5a3c1
rbrtj <robertjaszczurek@icloud.com>
2023-06-01 20:51:58 +0200

    Angular material installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PropertyCardComponent } from "./property/property-card/property-card.component";
-import { PropertyListComponent } from "./property/property-list/property-list.component";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        PropertyCardComponent,
-        PropertyListComponent,
-        NavBarComponent
-    ]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
